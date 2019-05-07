032204d4fcdd49a6506372d20f75c73fba3231c7
mythilymu <mudunurm@sheridancollege.ca>
2019-05-07 19:35:46 -0400

    practising with ejs

--- express_server.js
+++ express_server.js
@@ -8,8 +8,8 @@ const urlDatabase = {
 };
 
     //registers a handler on the root path, "/".
-app.get("/", (request, response) => {
-    response.send("Hello!");
+app.get("/", (req, res) => {
+    res.send("Hello!");
 });
 
 app.listen(PORT, () => {
@@ -17,10 +17,54 @@ app.listen(PORT, () => {
 })
 
 //Added Routes and endpoints
-app.get("/urls.json", (request, response) => {
-    response.json(urlDatabase);
+app.get("/urls.json", (req, res) => {
+    res.json(urlDatabase);
 });
 
-app.get("/hello", (request, response) => {
-    response.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/hello", (req, res) => {
+    res.send("<html><body>Hello <b>World</b></body></html>\n");
+}); 
+//When curl -i http://localhost:8080/hello
+/*Response: 
+HTTP/1.1 200 OK
+X-Powered-By: Express
+Content-Type: text/html; charset=utf-8
+Content-Length: 45
+ETag: W/"2d-+kq4PwugtS0rt17Ooq6yKzvojSE"
+Date: Tue, 07 May 2019 23:02:56 GMT
+Connection: keep-alive
+
+<html><body>Hello <b>World</b></body></html>
+*/
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page 
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+// about page 
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+app.listen(8080);
+console.log('8080 is the magic port');
+
+app.get('/', function(req, res) {
+    var drinks = [
+        { name: 'Bloody Mary', drunkness: 3 },
+        { name: 'Martini', drunkness: 5 },
+        { name: 'Scotch', drunkness: 10 }
+    ];
+    var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+
+    res.render('pages/index', {
+        drinks: drinks,
+        tagline: tagline
+    });
 });
