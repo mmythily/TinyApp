391e059e2520ecaf1b89c60cc52ddb3c023eb49e
Mythily <mythily.mudunuru@gmail.com>
2019-05-08 00:40:35 +0000

    added few routes, need code review to go forward

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+app.set("view engine", "ejs")
+
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
@@ -12,59 +14,37 @@ app.get("/", (req, res) => {
     res.send("Hello!");
 });
 
+
+//Added Routes and endpoints
+// app.get("/urls.json", (req, res) => {
+    //     res.json(urlDatabase);
+    // });
+    
+    // app.get("/hello", (req, res) => {
+        //     res.send("<html><body>Hello <b>World</b></body></html>\n");
+        // }); 
+        //When curl -i http://localhost:8080/hello
+        /*Response: 
+        HTTP/1.1 200 OK
+        X-Powered-By: Express
+        Content-Type: text/html; charset=utf-8
+        Content-Length: 45
+        ETag: W/"2d-+kq4PwugtS0rt17Ooq6yKzvojSE"
+        Date: Tue, 07 May 2019 23:02:56 GMT
+Connection: keep-alive
+*/
+
+//Rendering with EJS Template Engine and sending data to urls_index page
+app.get("urls", (req, res) => {
+    let templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+    let templateVars = { shortURL: req.params.shortURL, longURL: /* What goes here? */ };
+    res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 })
-
-//Added Routes and endpoints
-app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
-}); 
-//When curl -i http://localhost:8080/hello
-/*Response: 
-HTTP/1.1 200 OK
-X-Powered-By: Express
-Content-Type: text/html; charset=utf-8
-Content-Length: 45
-ETag: W/"2d-+kq4PwugtS0rt17Ooq6yKzvojSE"
-Date: Tue, 07 May 2019 23:02:56 GMT
-Connection: keep-alive
-
-<html><body>Hello <b>World</b></body></html>
-*/
-
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// use res.render to load up an ejs view file
-
-// index page 
-app.get('/', function(req, res) {
-    res.render('pages/index');
-});
-
-// about page 
-app.get('/about', function(req, res) {
-    res.render('pages/about');
-});
-
-app.listen(8080);
-console.log('8080 is the magic port');
-
-app.get('/', function(req, res) {
-    var drinks = [
-        { name: 'Bloody Mary', drunkness: 3 },
-        { name: 'Martini', drunkness: 5 },
-        { name: 'Scotch', drunkness: 10 }
-    ];
-    var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
-
-    res.render('pages/index', {
-        drinks: drinks,
-        tagline: tagline
-    });
-});
