bdb04e2daa2c1e2dea399f990a6bb67e017cc834
Mythily <mythily.mudunuru@gmail.com>
2019-05-10 19:45:52 +0000

    bugs with login handler

--- express_server.js
+++ express_server.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const cookieParser = require('cookie-parser')
-
-//body-parser library will convert the request body from a Buffer into string that we can read. 
-//It will then add the data to the req(request) object under the key body.
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080;
@@ -10,6 +7,7 @@ const PORT = 8080;
 app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
+
 
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
@@ -20,18 +18,28 @@ const users = {
     "userRandomID": {
         id: "userRandomID", 
         email: "user@example.com", 
-        password: "purple" //"purple-monkey-dinosaur"
+        password: "purple" 
     },
     "user2RandomID": {
         id: "user2RandomID", 
         email: "user2@example.com", 
-        password: "funk" //"dish-washer-funk"
+        password: "funk" 
     },
     "user2RandomIC": {
         id: "user2RandomID", 
         email: "user3@example.com", 
-        password: "funk" //"dish-washer-funk"
+        password: "punk" 
     }
+}
+
+generateRandomString = (strLength) => {
+    let randomString = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < strLength; i++ ) {
+        randomString += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return randomString;
 }
 
 emailLookup = (checkEmail) => {
@@ -43,24 +51,39 @@ emailLookup = (checkEmail) => {
     return false;
 }
 
+passwordLookup = (checkPassword) => {
+    for (let key in users){
+        if (users[key].password == checkPassword ){
+            return true;
+        }
+    }
+    return false;
+}
 
 //registers a handler on the root path, "/".
 app.get("/", (req, res) => {
-    res.send("Hello!");
+    res.send("Hello! Welcome to Tiny App");
 });
+
 
 // get/register endpoint
 app.get('/register', (req, res) => {
-    res.render("register");
+    let tempData = { 
+        urls: urlDatabase, 
+        user: users[req.cookies.user_id]
+    };
+    res.render("register", tempData);
 });
 
 
 app.post('/register', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
+    
     if (emailLookup(email) == true){
         res.status(400).send('400 Error! Email already exists. Go back and login');
-    } else if (!email || !password){
+    } 
+    else if (!email || !password){
         res.status(400).send('400 Error! Please enter a valid email and password');
     }
     else{
@@ -80,14 +103,39 @@ app.get('/login', (req, res) => {
     res.render("login");
 });
 
-//Working with cookies
+/*
+Update the POST /login endpoint to lookup the email address (submitted via the login from) in the user object.
+We needed this same functionality in the register route; if you created a function for looking up users by their email in that step, you can use that function again here! Your code will be so DRY it can dance underwater without getting wet.
+If a user with that e-mail cannot be found, return a response with a 403 status code.
+If a user with that e-mail address is located, compare the password given in the form with the existing user's password. If it does not match, return a response with a 403 status code.
+If both checks pass, set the user_id cookie with the matching user's random ID, then redirect to /urls.
+*/
 app.post('/login', (req, res) => {
     const user_id = req.body.user_id;
-    res.cookie('user_id', user_id);
-    res.redirect("/urls");
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    if (emailLookup(email) == false){
+        res.status(403).send('Forbidden : 403 Error! Email cannot be found');
+    } 
+    else if (passwordLookup(password) == false){
+        res.status(403).send('Forbidden : 403 Error! You entered a wrong password. Try Again');
+    }
+    else if (!email || !password){
+        res.status(400).send('Bad Request : 400 Error! Please enter a valid email and password');
+    }
+    else { //emailLookup(email) == true
+        email = req.body.email;
+        user_id = req.body.user_id;
+        res.cookie('user_id', user_id);
+        res.redirect("/urls");
+    }
 });
 
+
+
 app.post('/logout', (req, res) => {
+    
     res.clearCookie('user_id');
     res.redirect("/urls");
 });
@@ -95,22 +143,13 @@ app.post('/logout', (req, res) => {
 //Rendering with EJS Template Engine and sending data to urls_index page
 app.get("/urls", (req, res) => {
     let tempData = { 
-        user_id: req.body["user_id"],
         urls: urlDatabase, 
-        user : users[req.cookies.user_id]
+        user: users[req.cookies.user_id]
     };
+    console.log(tempData)
     res.render("urls_index", tempData);
 });
 
-function generateRandomString(strLength) {
-    let randomString = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    for ( let i = 0; i < strLength; i++ ) {
-        randomString += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return randomString;
-}
 
 app.post("/urls", (req, res) => {
     const shortURL = generateRandomString(6);
@@ -123,8 +162,10 @@ app.post("/urls", (req, res) => {
 app.get("/urls/new", (req, res) => {
     let tempData = { 
         user_id: req.cookies["user_id"],
-        urls: urlDatabase 
+        urls: urlDatabase,
+        user : users[req.cookies.user_id]
     };
+    console.log("Temp Database", tempData)
     res.render("urls_new", tempData);
 });
 
