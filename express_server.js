124a9c7873319eb0b37226db04875bc1e8c45d63
Mythily <mythily.mudunuru@gmail.com>
2019-05-17 15:45:56 +0000

    replaced unused code

--- express_server.js
+++ express_server.js
@@ -16,8 +16,8 @@ app.use(cookieSession({
     // req.session.user_id : req.session.user_id
 }))
 
-var urlDatabase = {};
-var users = {};
+const urlDatabase = {};
+const users = {};
 
 const generateRandomString = (strLength) => {
     let randomString = '';
